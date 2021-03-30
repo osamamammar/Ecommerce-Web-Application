aa065694ef60ec085d3a8cb917d2b4130d741568
Mahmoud Serag <sragmahmoud@gmail.com>
2021-03-30 16:33:08 +0200

    Build main structure of application
    - Add app.js file
    - Add config/db.js
    - Add controller directory to handle API
    - Add middlewares/async.js
    - Add models directory to handle database schema
    - Add public directory to save images and videos
    - Add routes directory to handle handle all routes
    - Add utils directory to handle error response
    - Add dependencies (express, bcrypt, dotenv, express-session, mongoose, morgan, nodemailer, nodemailer-sendgrid-transport and connect-mongodb-session) to application
    - Add devDependencies (cross-env and nodemon) to application

---
+++ Back Side/config/db.js
@@ -0,0 +1,22 @@
+// Load required packeges
+const mongoose = require('mongoose');
+
+// Connect to DB;
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    });
+  
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  }
+  catch (error) {
+    console.log(error.message);
+  }
+}
+
+// Export db file
+module.exports = connectDB;
---
+++ Back Side/middlewares/async.js
@@ -0,0 +1,5 @@
+// Replace {(try catch), (then catch)} in every middleware
+const asyncHandler = fn => (req, res, next) =>
+  Promise.resolve(fn(req, res, next)).catch(next);
+
+module.exports = asyncHandler;
