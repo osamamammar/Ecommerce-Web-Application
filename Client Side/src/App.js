cc812966f1aacb4fbd71d1fc911b4df722279393
osamamammar <osamaammar29@gmail.com>
2021-04-21 02:36:49 +0200

    impelement components  -footer  -header  -loader  -message alert  -product  -rating add products list reducer impelement screens  -home screen  -product screen

--- Client Side/src/App.js
+++ Client Side/src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Header></Header>
+      <main className="py-3">
+        <Container>
+          <Route path="/" component={HomeScreen} exact></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/cart/:id?" component={CartScreen}></Route>
+        </Container>
+      </main>
+      <Footer></Footer>
+    </Router>
+  );
 }
 
 export default App;
---
+++ Client Side/src/store.js
@@ -0,0 +1,21 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import {
+  productListReducer,
+  productDetailsReducer,
+} from "./reducers/productReducers";
+
+const rootReducer = combineReducers({
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+});
+const initialState = {};
+const middleware = [thunk];
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
