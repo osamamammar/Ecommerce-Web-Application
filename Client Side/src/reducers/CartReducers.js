7202d4ae97154e2a7cadc4b92d0935a8f0d02f09
osamamammar <osamaammar29@gmail.com>
2021-04-22 00:51:36 +0200

    add user login reducer and action

--- Client Side/src/reducers/CartReducers.js
+++ Client Side/src/reducers/CartReducers.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/CartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
