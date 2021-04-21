3a7b600fc34b3ebf5a2f329943650c50e1887328
osamamammar <osamaammar29@gmail.com>
2021-04-21 22:06:27 +0200

    cart screen
    remove items from cart

--- Client Side/src/actions/cartActions.js
+++ Client Side/src/actions/cartActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { CART_ADD_ITEM } from "../constants/CartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/CartConstants";
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
   const { data } = await axios.get(`/api/products/${id}`);
@@ -13,5 +13,13 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
       countInStock: data.countInStock,
     },
   });
-  localStorage.setItem("carItems", JSON.stringify(getState().cart.cartItems));
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
+
+export const removeFromCart = (id) => async (dispatch, getState) => {
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: id,
+  });
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 };
