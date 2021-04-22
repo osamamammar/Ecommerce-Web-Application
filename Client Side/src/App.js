530fef98ab25405803630215fbaf537b9f32d170
osamamammar <osamaammar29@gmail.com>
2021-04-22 13:25:24 +0200

    user register reducer,action and screen

--- Client Side/src/App.js
+++ Client Side/src/App.js
@@ -7,12 +7,14 @@ import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
 function App() {
   return (
     <Router>
       <Header></Header>
       <main className="py-3">
         <Container>
+          <Route path="/register" component={RegisterScreen}></Route>
           <Route path="/login" component={LoginScreen}></Route>
           <Route path="/product/:id" component={ProductScreen}></Route>
           <Route path="/cart/:id?" component={CartScreen}></Route>
