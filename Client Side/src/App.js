c113c055be12ba04da6c29723afc46707a4c86d0
osamamammar <osamaammar29@gmail.com>
2021-04-24 01:19:35 +0200

    add shipping screen and save address

--- Client Side/src/App.js
+++ Client Side/src/App.js
@@ -9,14 +9,16 @@ import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ProfileScreen from "./screens/ProfileScreen";
+import ShippingScreen from "./screens/ShippingScreen";
 function App() {
   return (
     <Router>
       <Header></Header>
       <main className="py-3">
         <Container>
-          <Route path="/register" component={RegisterScreen}></Route>
+          <Route path="/shipping" component={ShippingScreen}></Route>
           <Route path="/login" component={LoginScreen}></Route>
+          <Route path="/register" component={RegisterScreen}></Route>
           <Route path="/profile" component={ProfileScreen}></Route>
           <Route path="/product/:id" component={ProductScreen}></Route>
           <Route path="/cart/:id?" component={CartScreen}></Route>
