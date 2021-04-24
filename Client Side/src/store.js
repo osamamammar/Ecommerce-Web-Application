0caa4b5f3404aadbf95e80f3c5429b5979c5ebbd
osamamammar <osamaammar29@gmail.com>
2021-04-25 01:40:15 +0200

    add order pay reducer and action

--- Client Side/src/store.js
+++ Client Side/src/store.js
@@ -16,6 +16,7 @@ import {
 import {
   orderCreateReducer,
   orderDetailsReducer,
+  orderPayReducer,
 } from "./reducers/orderReducers";
 const rootReducer = combineReducers({
   productList: productListReducer,
@@ -27,6 +28,7 @@ const rootReducer = combineReducers({
   userUpdateProfile: userUpdateProfileReducer,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
+  orderPay: orderPayReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
