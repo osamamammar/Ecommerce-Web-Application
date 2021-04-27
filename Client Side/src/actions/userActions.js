cfae23e1669c6977e2274a5d73d48e784eaead40
osamamammar <osamaammar29@gmail.com>
2021-04-27 19:27:50 +0200

    add user edit screen and get user details

--- Client Side/src/actions/userActions.js
+++ Client Side/src/actions/userActions.js
@@ -174,7 +174,7 @@ export const deleteUser = (id) => async (dispatch, getState) => {
         Authorization: `Bearer ${userInfo.token}`,
       },
     };
-    const { data } = await axios.delete(`/api/users/${id}`, config);
+    await axios.delete(`/api/users/${id}`, config);
     dispatch({ type: USER_DELETE_SUCCESS });
   } catch (error) {
     dispatch({
